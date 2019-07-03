09ecc6a5a18f491e3d344015a2921a0b58c970fa
roee schmidt <Roee Schmidt>
2019-07-03 05:57:16 +0300

    added ppo implementation example from lunar-lander for referance...

---
+++ udacityDRL/deep-reinforcement-learning1/p2_continuous-control/Continuous_Control_PPO.py
@@ -0,0 +1,203 @@
+from unityagents import UnityEnvironment
+import numpy as np
+
+env = UnityEnvironment(file_name='Reacher_Linux/Reacher.x86_64')
+
+# get the default brain
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+
+
+# ### 2. Examine the State and Action Spaces
+# In this environment, a double-jointed arm can move to target locations. A reward of `+0.1` is provided for each step that the agent's hand is in the goal location. Thus, the goal of your agent is to maintain its position at the target location for as many time steps as possible.
+# The observation space consists of `33` variables corresponding to position, rotation, velocity, and angular velocities of the arm.  Each action is a vector with four numbers, corresponding to torque applicable to two joints.  Every entry in the action vector must be a number between `-1` and `1`.
+# Run the code cell below to print some information about the environment.
+
+# reset the environment
+env_info = env.reset(train_mode=True)[brain_name]
+
+# number of agents
+num_agents = len(env_info.agents)
+print('Number of agents:', num_agents)
+
+# size of each action
+action_size = brain.vector_action_space_size
+print('Size of each action:', action_size)
+
+# examine the state space 
+states = env_info.vector_observations
+state_size = states.shape[1]
+print('There are {} agents. Each observes a state with length: {}'.format(states.shape[0], state_size))
+print('The state for the first agent looks like:', states[0])
+
+
+
+# ### 3. Take Random Actions in the Environment
+# 
+# In the next code cell, you will learn how to use the Python API to control the agent and receive feedback from the environment.
+# 
+# Once this cell is executed, you will watch the agent's performance, if it selects an action at random with each time step.  A window should pop up that allows you to observe the agent, as it moves through the environment.  
+# 
+# Of course, as part of the project, you'll have to change the code so that the agent is able to use its experience to gradually choose better actions when interacting with the environment!
+
+if False:
+
+    env_info = env.reset(train_mode=False)[brain_name]     # reset the environment
+    states = env_info.vector_observations                  # get the current state (for each agent)
+    scores = np.zeros(num_agents)                          # initialize the score (for each agent)
+    while True:
+        actions = np.random.randn(num_agents, action_size) # select an action (for each agent)
+        actions = np.clip(actions, -1, 1)                  # all actions between -1 and 1
+        env_info = env.step(actions)[brain_name]           # send all actions to tne environment
+        next_states = env_info.vector_observations         # get next state (for each agent)
+        rewards = env_info.rewards                         # get reward (for each agent)
+        dones = env_info.local_done                        # see if episode finished
+        scores += env_info.rewards                         # update the score (for each agent)
+        states = next_states                               # roll over states to next time step
+        if np.any(dones):                                  # exit loop if episode finished
+            break
+    print('Total score (averaged over agents) this episode: {}'.format(np.mean(scores)))
+
+
+    # When finished, you can close the environment.
+    env.close()
+
+
+
+# Now it's your turn to train your own agent to solve the environment!  When training the environment, set `train_mode=True`, so that the line for resetting the environment looks like the following:
+# ```python
+# env_info = env.reset(train_mode=True)[brain_name]
+# ```
+import random
+import torch
+import numpy as np
+from collections import deque
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+import time
+from ddpg_agent import Agent
+agent = Agent(state_size=33, action_size=4, random_seed=2)
+
+def ddpg(n_episodes=5000, max_t=900, print_every=500, watch=False):
+    scores_deque = deque(maxlen=print_every)
+    scoresTot = []
+    for i_episode in range(1, n_episodes+1):
+        env_info = env.reset(train_mode=True)[brain_name]     # reset the environment
+        states = env_info.vector_observations                  # get the current state (for each agent)
+        # state = env.reset()
+        agent.reset()
+        scores = np.zeros(num_agents)                          # initialize the score (for each agent)
+        for t in range(max_t):
+            actions = agent.act(states)
+            #next_state, reward, done, _ = env.step(action)
+            env_info = env.step(actions)[brain_name]           # send all actions to tne environment
+            next_states = env_info.vector_observations         # get next state (for each agent)
+            rewards = env_info.rewards                         # get reward (for each agent)
+            dones = env_info.local_done                        # see if episode finished
+            agent.step(states, actions, rewards, next_states, dones)
+            states = next_states
+            scores += rewards
+            if watch:
+                time.sleep(0.1)
+            if dones[0]:
+                break 
+        scores_deque.append(scores)
+        scoresTot.append(scores)
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)), end="")
+        torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
+        torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
+        if i_episode % print_every == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))
+            print(actions)
+            
+    return scoresTot
+
+
+# In[8]:
+
+
+scores = ddpg()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(1, len(scores)+1), scores)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()
+
+
+# In[ ]:
+
+
+from parallelEnv import parallelEnv
+# keep track of how long training takes
+# WARNING: running through all 800 episodes will take 30-45 minute
+import ppo_utils
+# training loop max iterations
+episode = 500
+
+# widget bar to display progress
+# get_ipython().system('pip install progressbar')
+import progressbar as pb
+widget = ['training loop: ', pb.Percentage(), ' ', 
+          pb.Bar(), ' ', pb.ETA() ]
+timer = pb.ProgressBar(widgets=widget, maxval=episode).start()
+
+
+#envs = parallelEnv('PongDeterministic-v4', n=8, seed=1234)
+
+discount_rate = .99
+epsilon = 0.1
+beta = .01
+tmax = 320
+SGD_epoch = 4
+
+# keep track of progress
+mean_rewards = []
+
+for e in range(episode):
+
+    # collect trajectories
+    old_probs, states, actions, rewards =\
+        ppo_utils.collect_trajectories(envs, policy, tmax=tmax)  # TODO: continue here...
+        
+    total_rewards = np.sum(rewards, axis=0)
+
+
+    # gradient ascent step
+    for _ in range(SGD_epoch):
+        
+        # uncomment to utilize your own clipped function!
+        L = -ppo_utils.clipped_surrogate(policy, old_probs, states, actions, rewards, epsilon=epsilon, beta=beta)
+
+        optimizer.zero_grad()
+        L.backward()
+        optimizer.step()
+        del L
+    
+    # the clipping parameter reduces as time goes on
+    epsilon*=.999
+    
+    # the regulation term also reduces
+    # this reduces exploration in later runs
+    beta*=.995
+    
+    # get the average reward of the parallel environments
+    mean_rewards.append(np.mean(total_rewards))
+    
+    # display some progress every 20 iterations
+    if (e+1)%20 ==0 :
+        print("Episode: {0:d}, score: {1:f}".format(e+1,np.mean(total_rewards)))
+        print(total_rewards)
+        
+    # update progress widget bar
+    timer.update(e+1)
+    
+timer.finish()
+
+
+# In[ ]:
+
+
+
+
--- udacityDRL/deep-reinforcement-learning1/p2_continuous-control/ppo_utils.py
+++ udacityDRL/deep-reinforcement-learning1/p2_continuous-control/ppo_utils.py
@@ -1,4 +1,4 @@
-from parallelEnv import parallelEnv 
+from parallelEnv import parallelEnv
 import matplotlib
 import matplotlib.pyplot as plt
 import torch
@@ -238,32 +238,21 @@ def clipped_surrogate(policy, old_probs, states, actions, rewards,
     # this is desirable because we have normalized our rewards
     return torch.mean(clipped_surrogate + beta*entropy)
 
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 class Policy(nn.Module):
 
-    def __init__(self):
+    def __init__(self,state_size, action_size, fc1_units=400, fc2_units=300):
         super(Policy, self).__init__()
-        # 80x80x2 to 38x38x4
-        # 2 channel from the stacked frame
-        self.conv1 = nn.Conv2d(2, 4, kernel_size=6, stride=2, bias=False)
-        # 38x38x4 to 9x9x32
-        self.conv2 = nn.Conv2d(4, 16, kernel_size=6, stride=4)
-        self.size=9*9*16
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
         
-        # two fully connected layer
-        self.fc1 = nn.Linear(self.size, 256)
-        self.fc2 = nn.Linear(256, 1)
-
-        # Sigmoid to 
-        self.sig = nn.Sigmoid()
-        
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = x.view(-1,self.size)
-        x = F.relu(self.fc1(x))
-        return self.sig(self.fc2(x))
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        return F.tanh(self.fc3(x))  # was sigmoid!
     
